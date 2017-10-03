9cc0e5dfb1e1ce406d73e09cb9e13a4f065d2360
sixertoy <sixertoy.github@gmail.com>
2017-10-03 14:37:10 +0200

    update styles

--- src/app/actions/services.js
+++ src/app/actions/services.js
@@ -3,6 +3,11 @@
 import { parseString } from 'xml2js';
 import * as sortby from 'lodash.sortby';
 
+// application
+import { cutwords } from './../utils/cutwords';
+import { striptags } from './../utils/striptags';
+import { trimstring } from './../utils/trimstring';
+
 const loadingStart = () => ({
   type: 'onloadingstart'
 });
@@ -42,14 +47,21 @@ const makePromise = url => fetch(`${corsbridge}${url}`, fetchoptions)
   .then(body => new Promise(resolve =>
     parseString(body, (err, xml) => resolve(xml))));
 
+const shortendescription = (description) => {
+  let str = trimstring(description);
+  str = striptags(str);
+  return cutwords(str);
+};
+
 const parseResults = bodies =>
   bodies.reduce((acc, body) =>
     acc.concat(body.rss.channel[0].item), [])
-    .map(obj => ({
-      link: obj.link[0],
-      title: obj.title[0],
-      description: obj.description[0],
-      date: Date.parse(obj.pubDate[0])
+    .map(({ title, link, description, pubDate }) => ({
+      link: link[0],
+      title: title[0],
+      description: description[0],
+      date: Date.parse(pubDate[0]),
+      short: shortendescription(description[0])
     }));
 
 export const loadNewJobs = () => (dispatch, getstate) => {
--- src/app/components/KanbanColumn.js
+++ src/app/components/KanbanColumn.js
@@ -2,18 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // application
-import FeedItem from './FeedItem';
+import './kanbancolumn.css';
+import KanbanItem from './KanbanItem';
 
 const KanbanColumn = ({ title, items }) => (
   <div className="kanban-column">
-    <h2 className="kanban-column-title">
+    <h2 className="kanban-column-header">
       <span>{title}</span>
     </h2>
     <div className="kanban-column-items fancy-scrollbar">
       {items.map((obj, index) =>
         // eslint-disable-next-line react/no-array-index-key
-        <FeedItem {...obj} key={`kanban-column-item-${index}`} />)}
+        <KanbanItem {...obj} key={`kanban-column-item-${index}`} />)}
     </div>
+    <div className="kanban-column-footer" />
   </div>
 );
 
