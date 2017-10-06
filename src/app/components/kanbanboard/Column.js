eac71a358cbbbc850924c2c2510d89c7269e9542
sixertoy <sixertoy.github@gmail.com>
2017-10-06 08:57:34 +0200

    move list component into column as nested component

--- src/app/components/kanbanboard/Column.js
+++ src/app/components/kanbanboard/Column.js
@@ -1,23 +1,53 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import { DropTarget } from 'react-dnd';
+import React, { Component } from 'react';
 
 // application
 import './column.css';
-import List from './List';
+import Card from './Card';
 
-const BoardColumn = ({ title, items }) => (
-  <div className="board-column">
-    <h2 className="board-column-header">
-      <span>{title}</span>
-    </h2>
-    <List items={items} />
-    <div className="board-column-footer" />
-  </div>
-);
+class BoardColumn extends Component {
+  render () {
+    const {
+      title,
+      items,
+      isOver,
+      connectDropTarget } = this.props;
+    return connectDropTarget(
+      <div className="board-column">
+        <h2 className="board-column-header">
+          <span>{title}</span>
+        </h2>
+        <div className="board-column-list fancy-scrollbar"
+          style={{ height: 'auto', opacity: (isOver ? 0.45 : 1) }}>
+          {!items || !items.length
+            ? false
+            : items.map((obj, index) =>
+              // eslint-disable-next-line react/no-array-index-key
+              <Card {...obj} key={`kanban-column-item-${index}`} />)}
+        </div>
+        <div className="board-column-footer" />
+      </div>
+    );
+  }
+}
 
 BoardColumn.propTypes = {
   items: PropTypes.array.isRequired,
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  isOver: PropTypes.bool.isRequired,
+  connectDropTarget: PropTypes.func.isRequired
 };
 
-export default BoardColumn;
+const droptargetTarget = ({});
+
+const droptargetCollect = (connect, monitor) => ({
+  isOver: monitor.isOver(),
+  connectDropTarget: connect.dropTarget()
+});
+
+export default DropTarget(
+  'board-column',
+  droptargetTarget,
+  droptargetCollect
+)(BoardColumn);
