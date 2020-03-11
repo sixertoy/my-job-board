cdc72d391b55b082a98b0677feeb1e363d75ddbc
sixertoy <sixertoy.github@gmail.com>
2020-03-11 06:23:46 +0100

    remove old todolist component and update to new version with canCreate

--- src/app/components/napper-todolist/assets/index.js
+++
@@ -1,3 +0,0 @@
-export { default as NapperTodoListIconTrash } from './icon-trash';
-export { default as NapperTodoListIconChecked } from './icon-checked';
-export { default as NapperTodoListIconUnchecked } from './icon-unchecked';
--- src/app/components/napper-todolist/core/fp.js
+++
@@ -1,8 +0,0 @@
-export const noop = v => v;
-
-// TODO polyfill reduce/reduceRight
-export const pipe = (...funcs) => args =>
-  funcs.reduce((arg, fn) => fn(arg), args);
-
-export const compose = (...funcs) => args =>
-  funcs.reduceRight((acc, fn) => fn(acc), args);
